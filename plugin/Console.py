108be824421f9d04e81fc96a114df50909f30d67
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2024-08-21 20:47:48 +0200

    Update Console.py

--- plugin/Console.py
+++ plugin/Console.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# RAED & mfaraj57 &  (c) 2018
+
 from __future__ import print_function
 
 from enigma import eConsoleAppContainer
@@ -6,215 +10,166 @@ from Components.ActionMap import ActionMap
 from Components.ScrollLabel import ScrollLabel
 from Components.Sources.StaticText import StaticText
 from Screens.MessageBox import MessageBox
-from Components.Label import Label
+from enigma import getDesktop
+import sys
 
-# for locale (gettext)
-from . import _
+PY2 = sys.version_info[0] == 2
+PY3 = sys.version_info[0] == 3
+
+
+def getDesktopSize():
+    s = getDesktop(0).size()
+    return (s.width(), s.height())
+
+
+def isHD():
+    desktopSize = getDesktopSize()
+    return desktopSize[0] == 1280
 
 
 class Console(Screen):
+    if isHD():
+        skin = '''<screen position="17,center" size="1245,681" title="Command execution..." backgroundColor="#16000000" flags="wfNoBorder">
+            <widget name="text" position="9,48" size="1237,587" backgroundColor="#16000000" foregroundColor="#00ffffff" font="Console;24"/>
+            <eLabel text="Command execution..." font="Regular;30" size="1000,40" position="8,3" foregroundColor="#00ffffff" backgroundColor="#16000000" zPosition="4"/>
+            <eLabel position="10,674" size="165,5" backgroundColor="#00ff2525" zPosition="1"/>
+            <eLabel position="238,674" size="165,5" backgroundColor="#00389416" zPosition="1"/>
+            <eLabel position="1068,674" size="165,5" backgroundColor="#000080ff" zPosition="1"/>
+            <eLabel text="Cancel" position="10,646" zPosition="2" size="165,30" font="Regular;24" halign="center" valign="center" backgroundColor="#16000000" foregroundColor="#00ffffff" transparent="1"/>
+            <eLabel text="Hide/Show" position="238,646" zPosition="2" size="165,30" font="Regular;24" halign="center" valign="center" backgroundColor="#16000000" foregroundColor="#00ffffff" transparent="1"/>
+            <eLabel text="Restart GUI" position="1068,646" zPosition="2" size="165,30" font="Regular;24" halign="center" valign="center" backgroundColor="#16000000" foregroundColor="#00ffffff" transparent="1"/>
+        </screen>'''
+    else:
+        skin = '''<screen position="center,center" size="1886,1051" title="Command execution..." backgroundColor="#16000000" flags="wfNoBorder">
+            <widget name="text" position="9,93" size="1868,897" backgroundColor="#16000000" foregroundColor="#00ffffff" font="Console;33"/>
+            <eLabel text="Command execution..." font="Regular;45" size="1163,80" position="8,3" foregroundColor="#00ffffff" backgroundColor="#16000000" zPosition="4"/>
+            <eLabel position="10,1043" size="250,5" backgroundColor="#00ff2525" zPosition="1"/>
+            <eLabel position="353,1043" size="250,5" backgroundColor="#00389416" zPosition="1"/>
+            <eLabel position="1626,1043" size="250,5" backgroundColor="#000080ff" zPosition="1"/>
+            <eLabel text="Cancel" position="10,1004" zPosition="2" size="250,40" font="Regular;28" halign="center" valign="center" backgroundColor="#16000000" foregroundColor="#00ffffff" transparent="1"/>
+            <eLabel text="Hide/Show" render="Label" position="353,1004" zPosition="2" size="250,40" font="Regular;28" halign="center" valign="center" backgroundColor="#16000000" foregroundColor="#00ffffff" transparent="1"/>
+            <eLabel text="Restart GUI" position="1626,1004" zPosition="2" size="250,40" font="Regular;28" halign="center" valign="center" backgroundColor="#16000000" foregroundColor="#00ffffff" transparent="1"/>
+        </screen>'''
 
-    # cmdlist mat be a mixed list or tuple of strings
-    # or lists/tuples.
-    # Strings are executed by sh -c strng
-    # lists/tuples are executed by execvp(lst[0], lst)
-
-    def __init__(self, session, title="Console", cmdlist=None, finishedCallback=None, closeOnSuccess=False):
+    def __init__(self, session, title='Console', cmdlist=None, finishedCallback=None, closeOnSuccess=False, showStartStopText=True, skin=None):
         Screen.__init__(self, session)
-
         self.finishedCallback = finishedCallback
         self.closeOnSuccess = closeOnSuccess
+        self.showStartStopText = showStartStopText
+        if skin:
+            self.skinName = [skin, 'Console']
         self.errorOcurred = False
-
-        self["key_red"] = StaticText(_("Cancel"))
-        self["key_green"] = StaticText(_("hide"))
-
-        self["text"] = ScrollLabel("")
-        self["summary_description"] = StaticText("")
-        self["actions"] = ActionMap(["WizardActions", "DirectionActions", "ColorActions"],
-        {
-            "ok": self.cancel,
-            "back": self.cancel,
-            "up": self.key_up,
-            "down": self.key_down,
-            "green": self.key_green,
-            "red": self.key_red
-        }, -1)
-
-        self.cmdlist = cmdlist
-        self.newtitle = title
-
-        self.screen_hide = False
+        # lastpage = ''
+        self['text'] = ScrollLabel('')
+        # self['key_red'] = StaticText('Cancel')
+        # self['key_green'] = StaticText('Hide')
+        self["actions"] = ActionMap(["WizardActions", "DirectionActions", 'ColorActions'],
+                                    {
+                                    "ok": self.cancel,
+                                    "up": self["text"].pageUp,
+                                    "down": self["text"].pageDown,
+                                    "red": self.cancel,
+                                    "green": self.toggleHideShow,
+                                    "blue": self.restartenigma,
+                                    "cancel": self.cancel,
+                                    }, -1)
+        self.cmdlist = isinstance(cmdlist, list) and cmdlist or [cmdlist]
+        self.newtitle = title == 'Console' and ('Console') or title
         self.cancel_msg = None
-        self.output_file = ''
-
         self.onShown.append(self.updateTitle)
-
         self.container = eConsoleAppContainer()
         self.run = 0
-        self.container.appClosed.append(self.runFinished)
-        self.container.dataAvail.append(self.dataAvail)
-        self.onLayoutFinish.append(self.startRun)  # dont start before gui is finished
+        self.finished = False
+        try:
+            self.container.appClosed.append(self.runFinished)
+            self.container.dataAvail.append(self.dataAvail)
+        except:
+            self.container.appClosed_conn = self.container.appClosed.connect(self.runFinished)
+            self.container.dataAvail_conn = self.container.dataAvail.connect(self.dataAvail)
+        self.onLayoutFinish.append(self.startRun)
 
     def updateTitle(self):
         self.setTitle(self.newtitle)
 
-    def doExec(self, cmd):
-        if isinstance(cmd, (list, tuple)):
-            return self.container.execute(cmd[0], *cmd)
-        else:
-            return self.container.execute(cmd)
-
     def startRun(self):
-        self["text"].setText(_("Execution progress:") + "\n\n")
-        self["summary_description"].setText(_("Execution progress:"))
-        print("[Console] executing in run", self.run, " the command:", self.cmdlist[self.run])
-        if self.doExec(self.cmdlist[self.run]):  # start of container application failed...
-            self.runFinished(-1)  # so we must call runFinished manual
+        if self.showStartStopText:
+            self['text'].setText('Execution progress\n\n')
+        print('[Console] executing in run', self.run, ' the command:', self.cmdlist[self.run])
+        if self.container.execute(self.cmdlist[self.run]):
+            self.runFinished(-1)
 
     def runFinished(self, retval):
         if retval:
             self.errorOcurred = True
-            self.toggleScreenHide(True)
+            self.show()
         self.run += 1
         if self.run != len(self.cmdlist):
-            if self.doExec(self.cmdlist[self.run]):  # start of container application failed...
-                self.runFinished(-1)  # so we must call runFinished manual
+            if self.container.execute(self.cmdlist[self.run]):
+                self.runFinished(-1)
         else:
-            self["key_red"].setText(_("Close"))
-            self["key_green"].setText(_("Save"))
-            self.toggleScreenHide(True)
+            self.show()
+            self.finished = True
+            # try:
+                # lastpage = self['text'].isAtLastPage()
+            # except:
+                # lastpage = self['text']
             if self.cancel_msg:
                 self.cancel_msg.close()
-            lastpage = self["text"].isAtLastPage()
-            self["text"].appendText('\n' + _("Execution finished!!"))
-            self["summary_description"].setText('\n' + _("Execution finished!!"))
+            if self.showStartStopText:
+                self['text'].appendText('Execution finished!!')
             if self.finishedCallback is not None:
                 self.finishedCallback()
             if not self.errorOcurred and self.closeOnSuccess:
-                self.output_file = 'end'
-                self.cancel()
+                self.closeConsole()
+            else:
+                self['text'].appendText('\nPress OK or Exit to abort!')
+                # self['key_red'].setText('Exit')
+                # self['key_green'].setText('')
 
-    def key_up(self):
-        if self.screen_hide:
-            self.toggleScreenHide()
+    def toggleHideShow(self):
+        if self.finished:
             return
-        self["text"].pageUp()
-
-    def key_down(self):
-        if self.screen_hide:
-            self.toggleScreenHide()
-            return
-        self["text"].pageDown()
-
-    def key_green(self):
-        if self.screen_hide:
-            self.toggleScreenHide()
-            return
-        if self.output_file == 'end':
-            pass
-        elif self.output_file.startswith('/tmp/'):
-            self["text"].setText(self.readFile(self.output_file))
-            self["key_green"].setText('')
-            self.output_file = 'end'
-        elif self.run == len(self.cmdlist):
-            self.saveOutputText()
+        if self.shown:
+            self.hide()
         else:
-            self.toggleScreenHide()
+            self.show()
 
-    def key_red(self):
-        if self.screen_hide:
-            self.toggleScreenHide()
-            return
-        if self.run == len(self.cmdlist):
-            self.cancel()
+    def cancel(self):
+        if self.finished:
+            self.closeConsole()
         else:
-            self.cancel_msg = self.session.openWithCallback(self.cancelCB, MessageBox, _("Cancel execution?"), type=MessageBox.TYPE_YESNO, default=False, simple=True)
+            self.cancel_msg = self.session.openWithCallback(self.cancelCallback, MessageBox, 'Cancel execution?', type=MessageBox.TYPE_YESNO, default=False)
 
-    def cancelCB(self, ret=None):
+    def cancelCallback(self, ret=None):
         self.cancel_msg = None
         if ret:
-            self.cancel(True)
-
-    def saveOutputText(self):
-        from time import time, localtime
-        lt = localtime(time())
-        self.output_file = '/tmp/%02d%02d%02d_console.txt' % (lt[3], lt[4], lt[5])
-        self.session.openWithCallback(self.saveOutputTextCB, MessageBox, _("Save the commands and the output to a file?\n('%s')") % self.output_file, type=MessageBox.TYPE_YESNO, default=True, simple=True)
-
-    def formatCmdList(self, source):
-        if isinstance(source, (list, tuple)):
-            for x in source:
-                for y in self.formatCmdList(x):
-                    yield y
-        else:
-            yield source
-
-    def saveOutputTextCB(self, ret=None):
-        if ret:
-            from os import path
-            failtext = _("Path to save not exist: '/tmp/'")
-            if path.exists('/tmp/'):
-                text = 'commands ...\n\n'
-                try:
-                    cmdlist = list(self.formatCmdList(self.cmdlist))
-                    text += 'command line: %s\n\n' % cmdlist[0]
-                    script = ''
-                    for cmd in cmdlist[0].split():
-                        if '.' in cmd:
-                            if cmd[-3:] in ('.py', '.sh'):
-                                script = cmd
-                            break
-                    if script and path.isfile(script):
-                        text += 'script listing: %s\n\n%s\n\n' % (script, self.readFile(script))
-                    if len(cmdlist) > 1:
-                        text += 'next commands:\n\n' + '\n'.join(cmdlist[1:]) + '\n\n'
-                except:
-                    text += 'error read commands!!!\n\n'
-                text += '-' * 50 + '\n\noutputs ...\n\n%s' % self["text"].getText()
-                try:
-                    f = open(self.output_file, 'w')
-                    f.write(text)
-                    f.close()
-                    self["key_green"].setText(_("Load"))
-                    return
-                except:
-                    failtext = _("File write error: '%s'") % self.output_file
-            self.output_file = 'end'
-            self["key_green"].setText('')
-            self.session.open(MessageBox, failtext, type=MessageBox.TYPE_ERROR, simple=True)
-        else:
-            self.output_file = ''
-
-    def toggleScreenHide(self, setshow=False):
-        if self.screen_hide or setshow:
-            self.show()
-        else:
-            self.hide()
-        self.screen_hide = not (self.screen_hide or setshow)
-
-    def readFile(self, file):
-        try:
-            with open(file, 'r') as rdfile:
-                rd = rdfile.read()
-            rdfile.close()
-        except:
-            if file == self.output_file:
-                rd = self["text"].getText()
-            else:
-                rd = "File read error: '%s'\n" % file
-        return rd
-
-    def cancel(self, force=False):
-        if self.screen_hide:
-            self.toggleScreenHide()
-            return
-        if force or self.run == len(self.cmdlist):
+            try:  # DreamOS By RAED
+                self.container.appClosed.remove(self.runFinished)
+                self.container.dataAvail.remove(self.dataAvail)
+            except:
+                self.container.appClosed_conn = None
+                self.container.dataAvail_conn = None
+            self.container.kill()
             self.close()
-            self.container.appClosed.remove(self.runFinished)
-            self.container.dataAvail.remove(self.dataAvail)
-            if self.run != len(self.cmdlist):
-                self.container.kill()
+
+    def closeConsole(self):
+        if self.finished:
+            try:  # DreamOS By RAED
+                self.container.appClosed.remove(self.runFinished)
+                self.container.dataAvail.remove(self.dataAvail)
+            except:
+                self.container.appClosed_conn = None
+                self.container.dataAvail_conn = None
+            self.close()
+        else:
+            self.show()
 
     def dataAvail(self, str):
-        self["text"].appendText(str)
+        if PY3:
+            self['text'].appendText(str.decode())
+        else:
+            self['text'].appendText(str)
+
+    def restartenigma(self):
+        from Screens.Standby import TryQuitMainloop
+        self.session.open(TryQuitMainloop, 3)
